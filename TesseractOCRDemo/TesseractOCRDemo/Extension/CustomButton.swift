2e2e8853ab5387f63645e3988935a591eece3509
ami.solani <ami.s@simformsolutions.com>
2019-11-30 19:35:24 +0530

    UNT-T2695 Implement OCR prototype using tesseract for iphone or iPad  - initial commit

---
+++ TesseractOCRDemo/TesseractOCRDemo/Extension/CustomButton.swift
@@ -0,0 +1,42 @@
+//
+//  CustomButton.swift
+//  TesseractOCRDemo
+//
+//  Created by Ami Solani on 30/11/19.
+//  Copyright © 2019 Ami Solani. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+    
+        @IBInspectable var borderWidth: CGFloat {
+            set {
+                layer.borderWidth = newValue
+            }
+            get {
+                return layer.borderWidth
+            }
+        }
+        
+        @IBInspectable var cornerRadius: CGFloat {
+            set {
+                layer.cornerRadius = newValue
+            }
+            get {
+                return layer.cornerRadius
+            }
+        }
+        
+        @IBInspectable var borderColor: UIColor? {
+            set {
+                guard let uiColor = newValue else { return }
+                layer.borderColor = uiColor.cgColor
+            }
+            get {
+                guard let color = layer.borderColor else { return nil }
+                return UIColor(cgColor: color)
+            }
+        }
+    
+}
